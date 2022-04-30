889a80477e32a2632679a2fc0ad230a348b0995a
RealShiroTheShiro <RealShiroTheShiro@github.com>
2022-04-30 19:50:30 +0300

    Actively Tracking Rebel's Status

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 const RebelAFKBot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS", "GUILD_PRESENCES"] });
-const RebelID = '563178316362809357';
+const RebelID = '180899128006410240';
 const ServerID = '851740488674836520';
 
 const prefix = '+';
@@ -11,10 +11,60 @@ counter = 0;
 
 RebelAFKBot.once('ready', () => {
     console.log('RebelGuyMissingCounter is alive!');
-    if(typeof RebelAFKBot.guilds.cache.get(ServerID).members.cache.get(RebelID).presence?.status == 'undefined')
-        console.log('Rebel_guy is offline.');
+    if(typeof RebelAFKBot.guilds.cache.get(ServerID).members.cache.get(RebelID).presence?.status != 'undefined')
+        console.log('Rebel_guy is ' + RebelAFKBot.guilds.cache.get(ServerID).members.cache.get(RebelID).presence?.status);
     else
-    console.log('Rebel_guy is ' + RebelAFKBot.guilds.cache.get(ServerID).members.cache.get(RebelID).presence?.status);
+    {
+        console.log('Rebel_guy is offline.');
+        counting = true;
+        setInterval(() => {
+            counter++;
+            if(counter%24 === 1)
+                if(Math.round(counter/24) === 1)
+                    msg.channel.send('Rebel has been missing for 1 day and 1 hour!!');
+                else
+                    msg.channel.send('Rebel has been missing for '+ Math.round(counter/24) + ' days and 1 hour!!');
+            else if(Math.round(counter/24) === 1)
+                msg.channel.send('Rebel has been missing for 1 day and '+ counter%24 + ' hours!!');
+            else
+                msg.channel.send('Rebel has been missing for '+ Math.round(counter/24) + ' days and ' + counter%24 + ' hours!!');
+        }, 3600000);
+    }
+});
+
+RebelAFKBot.on('presenceUpdate', (oldPresence, newPresence) => {
+    let member = newPresence.member;
+    if (member.id === RebelID) {
+        if (oldPresence.status !== newPresence.status) {
+            let channel = member.guild.channels.cache.get('945301031347163147');
+
+            let text = "";
+
+            if (newPresence.status === "online") {
+                counting = false;
+                counter = 0;
+                text = "Rebel just went online!";
+            } else if (newPresence.status === "offline") {
+                counting = true;
+                text = "Rebel is offline again...";
+                channel.send("I hope to not see it");
+                setInterval(() => {
+                    counter++;
+                    if(counter%24 === 1)
+                        if(Math.round(counter/24) === 1)
+                            msg.channel.send('Rebel has been missing for 1 day and 1 hour!!');
+                        else
+                            msg.channel.send('Rebel has been missing for '+ Math.round(counter/24) + ' days and 1 hour!!');
+                    else if(Math.round(counter/24) === 1)
+                        msg.channel.send('Rebel has been missing for 1 day and '+ counter%24 + ' hours!!');
+                    else
+                        msg.channel.send('Rebel has been missing for '+ Math.round(counter/24) + ' days and ' + counter%24 + ' hours!!');
+                }, 3600000);        
+            }
+
+            channel.send(text);
+        }
+    }
 });
 
 
@@ -44,7 +94,7 @@ RebelAFKBot.on('messageCreate', (msg) => {
             msg.channel.send('Rebel_guy has been missing for '+ Math.round(counter/24) + ' days and ' + counter%24 + ' hours!!');
     }
 
-    else if(command === 'start')
+ /*   else if(command === 'start')
     {
         if(counting === true)
         {
@@ -87,8 +137,9 @@ RebelAFKBot.on('messageCreate', (msg) => {
         msg.channel.send('Reseting counter!');
         counter = 0;
     }
+    */
   });
 
 
 
-RebelAFKBot.login('OTY3MTU5MzIzODk5ODY3MjM2.YmMPTw.Jgq5kMt4KixDEeOoHlELd06NdRY');
+RebelAFKBot.login('');
